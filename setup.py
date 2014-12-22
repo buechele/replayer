e8ee02a25a40ffde40e504ab2d8b53ebba31e3de
Andreas Büchele <andreas.buechele@idealo.de>
2014-12-22 16:01:30 +0100

    some renamings

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name='replayreqs',
+setup(name='replayer',
       version='0.1',
-      description='Replays Apache access logs',
-      url='http://github.com/buechele/...',
+      description='Replay easily Apache access.logs',
+      url='https://github.com/buechele/replayer',
       author='Andreas Buechele',
       author_email='andreas@buechele.org',
       license='MIT',
-      packages=['replayreqs'],
-      scripts=['bin/replayreqs'],
+      packages=['replayer'],
+      scripts=['bin/replayer'],
       install_requires=[
             'apachelog',
       ],
